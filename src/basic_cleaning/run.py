aa3fe7661c89824b72ca078ccfade721d68d3f39
Chris Blythe <admin@pseudobrilliant.com>
2022-07-04 21:02:49 -0400

    Commiting updated cleaning to address new data issues found.

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,6 +29,10 @@ def go(args):
     df['last_review'] = pd.to_datetime(df['last_review'] )
     logging.info('Converted last review to date time')
 
+    # FIlter out rows with longitude and latitudes outside of the expected value
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     try:
         df.to_csv(args.output_artifact, index=False)
         logging.info('Exported clean dataframe to file location %s', args.output_artifact)
